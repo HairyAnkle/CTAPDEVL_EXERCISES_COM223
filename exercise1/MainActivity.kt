e4664000bb62857f9e7a56028cc182f835a8fa3c
Cristian P. Ogena <111488747+HairyAnkle@users.noreply.github.com>
2025-03-25 19:39:39 +0800

    Add files via upload

---
+++ exercise1/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.exercise1
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        var count = 0
+
+        button.setOnClickListener {
+            count++
+            val num = count.toString()
+            textView.text = num
+        }
+
+
+
+
+    }
+}
